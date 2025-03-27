e646a1ddff0ab5baef94f71e323b58088c37d9e0
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-03-27 16:01:52 -0300

    refactor Main class to enhance season and episode data retrieval and update ScreenmatchApplication to utilize Main for user interaction

--- screenmatch/src/main/java/br/com/jfabiodev/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/jfabiodev/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,9 @@
 package br.com.jfabiodev.screenmatch;
 
-import br.com.jfabiodev.screenmatch.model.DadosEpisodio;
-import br.com.jfabiodev.screenmatch.model.DadosSerie;
-import br.com.jfabiodev.screenmatch.model.DadosTemporadas;
-import br.com.jfabiodev.screenmatch.service.ConsumoAPI;
-import br.com.jfabiodev.screenmatch.service.ConverteDados;
+import br.com.jfabiodev.screenmatch.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,24 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-		String json = consumoAPI.obterDados("https://www.omdbapi.com/?t=mr.robot&apikey=f9173cff");
-		System.out.println(json);
-
-		ConverteDados converteDados = new ConverteDados();
-		DadosSerie dadosSerie = converteDados.obterDados(json,DadosSerie.class);
-		System.out.println(dadosSerie);
-
-		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=mr.robot&season=1&episode=2&apikey=f9173cff");
-		DadosEpisodio episodio = converteDados.obterDados(json,DadosEpisodio.class);
-		System.out.println(episodio);
-		List<DadosTemporadas> temporadas = new ArrayList<>();
-
-		for (int i = 1; i<= dadosSerie.totalTemporadas(); i++){
-			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=mr.robot&season="+i+"&apikey=f9173cff");
-			DadosTemporadas temporada = converteDados.obterDados(json, DadosTemporadas.class);
-			temporadas.add(temporada);
-		}
-		temporadas.forEach(System.out::println);
+		Main main = new Main();
+		main.exibeMenu();
 	}
 }
--- screenmatch/src/main/java/br/com/jfabiodev/screenmatch/main/Main.java
+++ screenmatch/src/main/java/br/com/jfabiodev/screenmatch/main/Main.java
@@ -1,12 +1,19 @@
 package br.com.jfabiodev.screenmatch.main;
 
+import br.com.jfabiodev.screenmatch.model.DadosEpisodio;
+import br.com.jfabiodev.screenmatch.model.DadosSerie;
+import br.com.jfabiodev.screenmatch.model.DadosTemporadas;
 import br.com.jfabiodev.screenmatch.service.ConsumoAPI;
+import br.com.jfabiodev.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     final Scanner scanner = new Scanner(System.in);
     private ConsumoAPI consumo = new ConsumoAPI();
+    private ConverteDados converteDados = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=f9173cff";
 
@@ -15,6 +22,25 @@ public class Main {
         String nomeSerie = scanner.nextLine();
         var consumoAPI = new ConsumoAPI();
         String json = consumo.obterDados(ENDERECO+ nomeSerie.replace(" ", "+") +API_KEY);
+        DadosSerie dadosSerie = converteDados.obterDados(json,DadosSerie.class);
+        System.out.println(dadosSerie);
 
+        List<DadosTemporadas> temporadas = new ArrayList<>();
+
+        for (int i = 1; i<= dadosSerie.totalTemporadas(); i++){
+            json = consumoAPI.obterDados(ENDERECO+ nomeSerie.replace(" ", "+")+"&season=" +i +API_KEY);
+            DadosTemporadas temporada = converteDados.obterDados(json, DadosTemporadas.class);
+            temporadas.add(temporada);
+        }
+        temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i < dadosSerie.totalTemporadas() ; i++) {
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemporada.size() ; j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t ->t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
