8ffc4e0c6a2c969afcfe8359248b60d8068f6e0a
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-04-10 09:48:32 -0300

    refactor exibeMenu method to remove commented code and add example of using streams for string manipulation

--- screenmatch/src/main/java/br/com/jfabiodev/screenmatch/main/Main.java
+++ screenmatch/src/main/java/br/com/jfabiodev/screenmatch/main/Main.java
@@ -7,6 +7,7 @@ import br.com.jfabiodev.screenmatch.service.ConsumoAPI;
 import br.com.jfabiodev.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,29 +19,34 @@ public class Main {
     private final String API_KEY = "&apikey=f9173cff";
 
     public void exibeMenu(){
-        System.out.println("Digite o nome da Série: ");
-        String nomeSerie = scanner.nextLine();
-        var consumoAPI = new ConsumoAPI();
-        String json = consumo.obterDados(ENDERECO+ nomeSerie.replace(" ", "+") +API_KEY);
-        DadosSerie dadosSerie = converteDados.obterDados(json,DadosSerie.class);
-        System.out.println(dadosSerie);
-
-        List<DadosTemporadas> temporadas = new ArrayList<>();
-
-        for (int i = 1; i<= dadosSerie.totalTemporadas(); i++){
-            json = consumoAPI.obterDados(ENDERECO+ nomeSerie.replace(" ", "+")+"&season=" +i +API_KEY);
-            DadosTemporadas temporada = converteDados.obterDados(json, DadosTemporadas.class);
-            temporadas.add(temporada);
-        }
-        temporadas.forEach(System.out::println);
-
-//        for (int i = 0; i < dadosSerie.totalTemporadas() ; i++) {
-//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-//            for (int j = 0; j < episodiosTemporada.size() ; j++) {
-//                System.out.println(episodiosTemporada.get(j).titulo());
-//            }
+//        System.out.println("Digite o nome da Série: ");
+//        String nomeSerie = scanner.nextLine();
+//        var consumoAPI = new ConsumoAPI();
+//        String json = consumo.obterDados(ENDERECO+ nomeSerie.replace(" ", "+") +API_KEY);
+//        DadosSerie dadosSerie = converteDados.obterDados(json,DadosSerie.class);
+//        System.out.println(dadosSerie);
+//
+//        List<DadosTemporadas> temporadas = new ArrayList<>();
+//
+//        for (int i = 1; i<= dadosSerie.totalTemporadas(); i++){
+//            json = consumoAPI.obterDados(ENDERECO+ nomeSerie.replace(" ", "+")+"&season=" +i +API_KEY);
+//            DadosTemporadas temporada = converteDados.obterDados(json, DadosTemporadas.class);
+//            temporadas.add(temporada);
 //        }
+//        temporadas.forEach(System.out::println);
+//
+////        for (int i = 0; i < dadosSerie.totalTemporadas() ; i++) {
+////            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+////            for (int j = 0; j < episodiosTemporada.size() ; j++) {
+////                System.out.println(episodiosTemporada.get(j).titulo());
+////            }
+////        }
 
-        temporadas.forEach(t ->t.episodios().forEach(e -> System.out.println(e.titulo())));
+    //    temporadas.forEach(t ->t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<String> nomes = Arrays.asList("Fabio","Guimarães","Filho");
+        nomes.stream().sorted().limit(3).filter(n -> n.startsWith("G"))
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
     }
 }
