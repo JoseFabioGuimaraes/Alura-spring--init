f727b2518f93cf49f77da0089563024fbdd0845f
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-04-11 11:27:08 -0300

    remove unused import for Collector in Main class

--- atividade1/src/main/java/Atividade3/Main.java
+++ atividade1/src/main/java/Atividade3/Main.java
@@ -3,7 +3,6 @@ package Atividade3;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main {
