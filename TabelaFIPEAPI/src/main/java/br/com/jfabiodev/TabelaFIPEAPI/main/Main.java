d3332f2efc238fcb67d714dc86b9fb9ec54417c7
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-04-16 16:11:19 -0300

    add vehicle filtering and details retrieval functionality

--- TabelaFIPEAPI/src/main/java/br/com/jfabiodev/TabelaFIPEAPI/main/Main.java
+++ TabelaFIPEAPI/src/main/java/br/com/jfabiodev/TabelaFIPEAPI/main/Main.java
@@ -2,11 +2,15 @@ package br.com.jfabiodev.TabelaFIPEAPI.main;
 
 import br.com.jfabiodev.TabelaFIPEAPI.model.Dados;
 import br.com.jfabiodev.TabelaFIPEAPI.model.Modelos;
+import br.com.jfabiodev.TabelaFIPEAPI.model.Veiculo;
 import br.com.jfabiodev.TabelaFIPEAPI.service.ConsomeAPI;
 import br.com.jfabiodev.TabelaFIPEAPI.service.ConverteDados;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -57,5 +61,34 @@ public class Main {
         modeloLista.modelos().stream()
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
+
+        System.out.println("Digite um trecho do nome do carro a ser buscado: ");
+        var nomeVeiculo = scanner.nextLine();
+
+        List<Dados> modelosFiltrados = modeloLista.modelos().stream()
+                .filter(m -> m.nome().toUpperCase().contains(nomeVeiculo.toUpperCase()))
+                .collect(Collectors.toList());
+
+        System.out.println("Modelos Filtrados: \n");
+        modelosFiltrados.forEach(System.out::println);
+
+        System.out.println("Digite o código do modelo desejado: ");
+        var codVeiculo = scanner.nextLine();
+
+        endereco = endereco+ "/"+codVeiculo + "/anos";
+        json = consumoAPI.obterDados(endereco);
+
+        List<Dados> anos = conversorDados.obterLista(json, Dados.class);
+        List<Veiculo> veiculos = new ArrayList<>();
+
+        for (Dados ano : anos) {
+            var enderecoAnos = endereco + "/" + ano.codigo();
+            json = consumoAPI.obterDados(enderecoAnos);
+            Veiculo veiculo = conversorDados.obterDados(json, Veiculo.class);
+            veiculos.add(veiculo);
+        }
+
+        System.out.println("Todos os veículos filtrados avaliação por anos: ");
+        veiculos.forEach(System.out::println);
     }
 }
